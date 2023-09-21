1fd83bc9a2233e5763b194fe34bf645df0ce241c
[Vamsi Duvvuri] <vamsi.duvvuri@icloud.com>
2023-09-21 18:10:19 -0400

    worker running in bckgrnd

--- app.py
+++ app.py
@@ -1,16 +1,39 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, render_template, redirect, url_for
 from redis import Redis
 from rq import Queue
-from tasks import background_task  # Change is here
+from tasks import background_task
 
 app = Flask(__name__)
-app.config['REDIS_URL'] = "redis://localhost:6379/0"
-redis = Redis.from_url(app.config['REDIS_URL'])
-q = Queue('default', connection=redis)
+app.config["REDIS_URL"] = "redis://localhost:6379/0"
+redis = Redis.from_url(app.config["REDIS_URL"])
+q = Queue("default", connection=redis)
 
-@app.route('/task', methods=['POST'])
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+
+@app.route("/task", methods=["POST"])
 def add_task():
-    data = request.get_json()
-    task_name = data.get('name')
+    task_name = request.form["name"]
     job = q.enqueue(background_task, task_name)
-    return jsonify
+    return redirect(url_for("get_results", job_key=job.id))
+
+
+@app.route("/results/<job_key>", methods=["GET"])
+def get_results(job_key):
+    try:
+        job = q.fetch_job(job_key)
+        if job:
+            return render_template(
+                "job_status.html", status=job.get_status(), result=job.result
+            )
+        else:
+            return "No such job."
+    except Exception as e:
+        return str(e)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
